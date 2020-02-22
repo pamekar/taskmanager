87c698372f0d4397daff00034da37b5b6e6e6f5b
pamekar <d.pamekar@gmail.com>
2020-02-22 14:53:10 +0100

    DB migrations for tasks

---
+++ database/migrations/2020_02_22_133058_create_task_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks_users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('task_id');
+            $table->string('user_id');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks_users');
+    }
+}
