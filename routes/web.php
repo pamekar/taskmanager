347caae1d3e185182f82b7e001625c6d834bb963
pamekar <d.pamekar@gmail.com>
2020-02-22 14:55:39 +0100

    Setup Auth

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
+use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
