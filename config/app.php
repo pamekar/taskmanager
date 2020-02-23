85088a44f8908128b60b2aa3f5f90d812cc07cb4
pamekar <d.pamekar@gmail.com>
2020-02-23 23:27:32 +0100

    JWT config setup

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Tymon\JWTAuth\Facades\JWTAuth;
+
 return [
 
     /*
@@ -174,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
     ],
 
@@ -225,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'JWTAuth' => JWTAuth::class
     ],
 
 ];
