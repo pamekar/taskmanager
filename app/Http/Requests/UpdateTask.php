b57df0661245e5b4e803215321caa23d73cf04b6
pamekar <d.pamekar@gmail.com>
2020-02-23 23:51:17 +0100

    Form request validators

---
+++ app/Http/Requests/UpdateTask.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTask extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'string|max:255',
+            'description' => 'string|max:255',
+            'start_at' => 'nullable|date',
+            'end_at' => 'nullable|date'
+        ];
+    }
+}
