5732f7b682ea1a9dcf16bbf0455c8bbeb182d884
pamekar <d.pamekar@gmail.com>
2020-02-23 23:27:05 +0100

    JWT Authentication setup

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,15 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('tasks', 'TaskController')->middleware('jwt.verify');
-
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function () {
-    Route::post('login', 'AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
-
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
